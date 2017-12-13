d346a4b0f389b555b52fff15c6c2ada3d0c92f70
konev <konev@konev>
2017-12-13 13:03:03 +0300

    изменил hid дескриптор self powered -> bus powered

--- STM32_USB_Device_Library/Class/CustomHID/Src/usbd_customhid.c
+++ STM32_USB_Device_Library/Class/CustomHID/Src/usbd_customhid.c
@@ -144,7 +144,7 @@ __ALIGN_BEGIN static uint8_t USBD_CUSTOM_HID_CfgDesc[USB_CUSTOM_HID_CONFIG_DESC_
   0x01,         /*bConfigurationValue: Configuration value*/
   0x00,         /*iConfiguration: Index of string descriptor describing
   the configuration*/
-  0xC0,         /*bmAttributes: bus powered */
+  0xA0,         /*bmAttributes: bus powered */
   0x32,         /*MaxPower 100 mA: this current is used for detecting Vbus*/
   
   /************** Descriptor of CUSTOM HID interface ****************/
